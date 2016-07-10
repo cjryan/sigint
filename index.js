14329cfebff741ad8c509212756c733c86ced8b7
Chris Ryan <cjryan@email.arizona.edu>
2016-07-10 00:12:15 -0500

    Added link capture capability.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var self = require("sdk/self");
+var data = require("sdk/self").data;
+var pageMod = require("sdk/page-mod");
 
 //https://developer.mozilla.org/en-US/Add-ons/SDK/High-Level_APIs/hotkeys
 // Define keyboard shortcuts for showing and hiding a custom panel.
@@ -8,6 +10,12 @@ var panel = require("sdk/panel").Panel({
   width: 180,
   height: 180,
   contentURL: "https://en.wikipedia.org/w/index.php?title=Jetpack&useformat=mobile"
+});
+
+//Modify the page to allow jquery, and link identification
+pageMod.PageMod({
+  include: "*",
+  contentScriptFile: [data.url("jquery-3.0.0.min.js"), data.url("link_click.js")]
 });
 
 var showHotKey = Hotkey({
