89d54710e3e38ada6fa4e2d4c7abffe2ec59c538
Chris Ryan <cjryan@email.arizona.edu>
2016-07-14 20:57:12 -0500

    Current braindump.

--- data/link_click.js
+++ data/link_click.js
@@ -1,19 +1,30 @@
 //http://stackoverflow.com/questions/3872498/jquery-how-to-catch-keydown-mouse-click-event/3872560#3872560
 $(function() {
   $("a").click(function(event) {
+    //create an object to store the link data
+    var link_metadata = {};
+
     //jquery click event can use altKey, ctrlKey, key, keyCode, metaKey, shiftKey
     //https://api.jquery.com/category/events/event-object/
     if (event.altKey && event.ctrlKey && event.shiftKey) {
       console.log("works.");
     }
     else if (event.altKey && event.ctrlKey) {
-      console.log(event.key);
-      console.log('Link Text: ' + $(this).text());
-      console.log('Link Hypertext: ' + $(this).attr("href"));
-      console.log('Link Surrounding Text: ' + $(this).parent().text());
-      console.log('Current page where link lives: ' + window.location.href);
-      console.log('Referring Document: ' + document.referrer);
+      //Add checking logic, if var is blank, add empty strings for json
+      current_time = new Date($.now());
+      link_text = $(this).text();
+      link_metadata["link_text"] = link_text;
+      href_text = $(this).attr("href");
+      link_metadata["href_text"] = href_text;
+      surr_text = $(this).parent().text();
+      link_metadata["surr_text"] = surr_text;
+      curr_page = window.location.href;
+      link_metadata["curr_page"] = curr_page;
+      ref_page = document.referrer;
+      link_metadata["ref_page"] = ref_page;
+      link_metadata["curr_time"] = current_time;
     }
+    self.port.emit("link_entered", link_metadata);
   });
 });
 
