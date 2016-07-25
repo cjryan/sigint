37b0cbd945fade4c68bef4a7cf68ae5b7201ec2c
Chris Ryan <cjryan@email.arizona.edu>
2016-07-25 00:46:21 -0500

    Added badge, added functionality to change topic.

--- data/set_topic.js
+++ data/set_topic.js
@@ -3,9 +3,10 @@
 self.port.on("curr_topic_contents", function getTopicContents(topic) {
   if (topic !== null) {
     //show the current topic panel to the user, if already set
-    console.log("Current topic is..." + topic);
     $("#topic_contents").html(topic);
     $("#set_current_topic").css("display","none");
+    $("#current_topic").css("display","block");
+    $("#new_topic").val('');
   }
 });
 
@@ -13,4 +14,9 @@ self.port.on("curr_topic_contents", function getTopicContents(topic) {
 $('#set_topic').click(function(){
   var topic_name = $('#topic_name').val();
   self.port.emit("topic_entered", topic_name);
+});
+
+$('#change_topic').click(function(){
+  var change_topic_name = $('#new_topic').val();
+  self.port.emit("topic_entered", change_topic_name);
 });
