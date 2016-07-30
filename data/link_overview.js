fd509eafcf7ad97e97d0f7722c9566af8876db4c
Chris Ryan <cjryan@email.arizona.edu>
2016-07-30 17:08:25 -0500

    Link html cleanup.

--- data/link_overview.js
+++ data/link_overview.js
@@ -16,16 +16,16 @@ self.port.on("send_link_pile", function sortLinks(link_pile) {
         "<li><a href='#' class='link_ref'>"
         + link_title + "</a>";
       var hid_div = "<div class='link_meta' style='display: none;'>" +
-        "<ul><li id='link_id'>id: " +
-        link_arr[i].id + "</li><li>link text: " +
-        link_arr[i].link_text + "</li><li>href text: " +
-        link_arr[i].href_text + "</li><li>surrounding text: " +
-        link_arr[i].surr_text + "</li><li>current page: " +
-        link_arr[i].curr_page + "</li><li>referring page: " +
-        link_arr[i].ref_page + "</li><li>link capture time: " +
-        link_arr[i].curr_time + "</li><li>current topic: " +
-        link_arr[i].curr_topic + "</li><li>notes: " +
-        link_arr[i].notes + "</li><li><a href='#' class='edit' onclick='testfunction();'>edit</a></li></ul></div></li><br /><br />";
+        "<ul><li id='link_id'>id: " + link_arr[i].id +
+        "</li><li>link text: " + link_arr[i].link_text +
+        "</li><li>href text: " + link_arr[i].href_text +
+        "</li><li>surrounding text: " + link_arr[i].surr_text +
+        "</li><li>current page: " +  link_arr[i].curr_page +
+        "</li><li>referring page: " + link_arr[i].ref_page +
+        "</li><li>link capture time: " + link_arr[i].curr_time +
+        "</li><li>current topic: " + link_arr[i].curr_topic +
+        "</li><li>notes: " +  link_arr[i].notes +
+        "</li><li><a href='#' class='edit' onclick='testfunction();'>edit</a></li></ul></div></li><br /><br />";
         midlist = midlist + link_list_item + hid_div;
   }
   var full_list = startlist + midlist + endlist;
